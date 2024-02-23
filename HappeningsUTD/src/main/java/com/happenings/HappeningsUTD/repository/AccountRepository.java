4d43b8bf5614f7265ac477107e133f44e2eabc0e
Jonathan Lam <106890961+jonathan-jlam@users.noreply.github.com>
2024-02-23 12:08:51 -0600

    Backend to database using JPA

---
+++ HappeningsUTD/src/main/java/com/happenings/HappeningsUTD/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.happenings.HappeningsUTD.repository;
+
+import com.happenings.HappeningsUTD.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+}
+    
+
