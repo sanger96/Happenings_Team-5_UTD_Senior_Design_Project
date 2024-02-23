45073f11a76a0f41b64c634360586761079203d0
Jonathan Lam <106890961+jonathan-jlam@users.noreply.github.com>
2024-02-23 12:21:44 -0600

    Backend to Database Communication JPA

---
+++ HappeningsUTD/src/test/java/com/services/api/HappeningsUtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happenings.HappeningsUTD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappeningsUtdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
