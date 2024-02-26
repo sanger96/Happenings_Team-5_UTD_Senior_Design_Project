b2c40eb8480303e372258b0da795eb7cb22e1761
Robert Dohm <Robert.Dohm@utdallas.edu>
2024-02-26 14:13:44 -0600

    new file:   .vscode/settings.json 	new file:   SpringBootBackend/src/main/java/com/services/api/controller/LocationController.java 	new file:   SpringBootBackend/src/main/java/com/services/api/entity/Location.java     new file:   SpringBootBackend/src/main/java/com/services/api/repository/LocationRepository.java 	new file:   SpringBootBackend/src/main/java/com/services/api/service/LocationService.java

---
+++ SpringBootBackend/src/main/java/com/services/api/controller/LocationController.java
@@ -0,0 +1,29 @@
+package com.services.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.services.api.entity.Location;
+import com.services.api.service.LocationService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+public class LocationController {
+    
+    @Autowired
+    private LocationService service;
+
+    @PostMapping("/addLocation")
+    public Location addAccount(@RequestBody Location location) {
+        return service.saveAccount(location);
+    }
+
+    @GetMapping("/getLocationById/{id}")
+    public Location getAccountById(@PathVariable int id) {
+        return service.getLocationById(id);
+    }
+}
---
+++ SpringBootBackend/src/main/java/com/services/api/service/LocationService.java
@@ -0,0 +1,21 @@
+package com.services.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.services.api.entity.Location;
+import com.services.api.repository.LocationRepository;
+
+@Service
+public class LocationService {
+    @Autowired
+    private LocationRepository repository;
+
+    public Location saveAccount(Location location) {
+        return repository.save(location);
+    }
+
+    public Location getLocationById(int id) {
+        return repository.findById(id).orElse(null);
+    }
+}
