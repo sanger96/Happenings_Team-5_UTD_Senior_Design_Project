aa2a331b0750c117aa91e94139492fece16650c6
Robert Dohm <Robert.Dohm@utdallas.edu>
2024-02-26 19:50:44 -0600

    new file:   SpringBootBackend/src/main/java/com/services/api/controller/ClubController.java 	new file:   SpringBootBackend/src/main/java/com/services/api/controller/LocationController.java 	new file:   SpringBootBackend/src/main/java/com/services/api/entity/Club.java 	new file:   SpringBootBackend/src/main/java/com/services/api/entity/Location.java 	new file:   SpringBootBackend/src/main/java/com/services/api/repository/ClubRepository.java 	new file:   SpringBootBackend/src/main/java/com/services/api/repository/LocationRepository.java 	new file:   SpringBootBackend/src/main/java/com/services/api/service/ClubService.java 	new file:   SpringBootBackend/src/main/java/com/services/api/service/LocationService.java

---
+++ SpringBootBackend/src/main/java/com/services/api/repository/ClubRepository.java
@@ -0,0 +1,10 @@
+package com.services.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.services.api.entity.Club;
+
+public interface ClubRepository extends JpaRepository<Club, Integer> {
+
+}
+    
