dfba322dcc8f981c3d9c388d6f49ee170e171645
gaurav kumar sanger <gauravkumarsanger@gmail.com>
2024-03-04 22:51:22 -0600

    got rid of three dots in top right of my screen

--- HappeningsApp/app/src/main/java/com/example/happeningsapp/MainActivity.java
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
+        //getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
         return true;
     }
 
