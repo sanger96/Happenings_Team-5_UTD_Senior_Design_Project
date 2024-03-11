9b6aa39c2f73f242a52ddc0d5023622c49ab4086
LKASTI <lkastiman@gmail.com>
2024-03-11 08:25:12 -0500

    Added componentScan annotation for all services

--- SpringBootBackend/src/main/java/com/services/api/HappeningsUtdApplication.java
+++ SpringBootBackend/src/main/java/com/services/api/HappeningsUtdApplication.java
@@ -2,8 +2,10 @@ package com.services.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.services"})
 public class HappeningsUtdApplication {
 
 	public static void main(String[] args) {
