c67dc1e504963af0fabe273f1259f0e816681c84
gaurav kumar sanger <gauravkumarsanger@gmail.com>
2024-03-19 14:25:40 -0500

    added login page and starts app on that page. Does not yet navigate to next page upon successful login attempt.

--- HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/login/LoginViewModel.java
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/login/LoginViewModel.java
@@ -1,7 +1,35 @@
 package com.example.happeningsapp.ui.login;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private final MutableLiveData<String> topPrompt;
+    private final MutableLiveData<String> username;
+    private final MutableLiveData<String> password;
+
+    public LoginViewModel() {
+        topPrompt = new MutableLiveData<>();
+        //sets top prompt
+        topPrompt.setValue("Login settings page\nFor successful login attempt use default values");
+        username = new MutableLiveData<>();
+        //sets username
+        username.setValue("Enter username");
+        password = new MutableLiveData<>();
+        //sets password
+        password.setValue("Enter password");
+
+    }
+
+    public LiveData<String> getText() {
+        return topPrompt;
+    }
+    public LiveData<String> getUsername() {
+        return username;
+    }
+    public LiveData<String> getPassword() {
+        return password;
+    }
 }
