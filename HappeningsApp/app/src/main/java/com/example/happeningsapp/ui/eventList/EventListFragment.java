25c63a1da6d9446198ff832058858c7814d33d1b
Gaurav <gauravkumarsanger@gmail.com>
2024-03-25 14:20:36 -0500

    attempting to add iframe for events list. app load, but page does not.

--- HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/eventList/EventListFragment.java
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/eventList/EventListFragment.java
@@ -1,15 +1,19 @@
 package com.example.happeningsapp.ui.eventList;
 
+import static androidx.databinding.DataBindingUtil.setContentView;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.happeningsapp.R;
 import com.example.happeningsapp.databinding.FragmentEventListBinding;
 
 public class EventListFragment extends Fragment {
@@ -24,10 +28,15 @@ public class EventListFragment extends Fragment {
         binding = FragmentEventListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        EventListViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //webView for iframe on event list page
+        WebView myWebView = new WebView(binding.webView.getContext());
+        //myWebView.getSettings().setJavaScriptEnabled(true);
+        setContentView(myWebView);
+        myWebView.loadUrl("https://www.google.com");
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
