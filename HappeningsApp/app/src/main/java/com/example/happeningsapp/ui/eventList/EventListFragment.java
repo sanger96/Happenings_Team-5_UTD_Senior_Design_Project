877c8fecf50e94f5dfb80a0131c90d77d0ea0f22
gaurav kumar sanger <gauravkumarsanger@gmail.com>
2024-03-28 15:05:31 -0500

    added table layout to fragment_event_list.xml added baisc http get method to EventListFragment; doesn't work yet. Fails.

--- HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/eventList/EventListFragment.java
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/eventList/EventListFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 //import android.webkit.WebView;
 //import android.widget.TextView;
 
@@ -14,7 +15,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 //import com.example.happeningsapp.R;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.happeningsapp.databinding.FragmentEventListBinding;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class EventListFragment extends Fragment {
 
@@ -27,10 +38,34 @@ public class EventListFragment extends Fragment {
 
         binding = FragmentEventListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        TextView topElement = binding.topElement;
 
-        //use get method grab events list and populate a table
-        //http://localhost:<port>/event/getAll
+        //URL to get from
+        String getUrl="http://localhost:10.0.2.2/event/getAll";
+        RequestQueue requestQueue = Volley.newRequestQueue(root.getContext());
+        // since the response we get from the api is in JSON,
+        // we need to use `JsonObjectRequest` for
+        // parsing the request response
+        // Request a string response from the provided URL.
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, getUrl, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        topElement.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        topElement.setText("Failed");
+
+                    }
+                });
+        //add to queue
+        requestQueue.add(jsonObjectRequest);
         return root;
     }
 
