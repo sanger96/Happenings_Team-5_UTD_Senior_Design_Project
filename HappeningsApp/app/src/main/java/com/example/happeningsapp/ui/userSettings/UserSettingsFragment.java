3c9f4c4a9584d19b65e05c83c3d2302b004b3722
gaurav kumar sanger <gauravkumarsanger@gmail.com>
2024-03-27 22:43:43 -0500

    added ability to post with plain text in manifest. Added volley method to post for new account on UserSettingsFragment NOTE: using port 10.0.2.2 because this will map to local host

--- HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/userSettings/UserSettingsFragment.java
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/userSettings/UserSettingsFragment.java
@@ -1,6 +1,7 @@
 package com.example.happeningsapp.ui.userSettings;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +14,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.happeningsapp.R;
 import com.example.happeningsapp.databinding.FragmentUserSettingsBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class UserSettingsFragment extends Fragment {
@@ -51,16 +61,71 @@ public class UserSettingsFragment extends Fragment {
             @Override
             public void onClick(View view){
                 //TODO add Post method here
-                //if statement for passing email and password to back end to create account
-                //http://localhost:<3306>/useraccount/add
+                Log.i("UserSettingsFragment","this is in onClick");
+                //volleyPostAddAccount(email.getText().toString(),password.getText().toString());
+                String postUrl="http://10.0.2.2:8080/account/add";
+                RequestQueue requestQueue = Volley.newRequestQueue(root.getContext());
 
+                JSONObject postData = new JSONObject();
+                try{
+                    postData.put("email",email.getText().toString());
+                    postData.put("password", password.getText().toString());
 
+                    Log.i("UserSettingsFragment","this is try");
+                } catch (JSONException e){
+                    Log.i("UserSettingsFragment","this is catch");
+                    e.printStackTrace();
+                }
+//                JSONObject postData = JSONObject.quote({ "email": "VScode" , "password": "testing from VScode" });
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,postUrl,postData, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //add toast for success
+                        //Toast.makeText(UserSettingsFragment.getContext(), "Post Successful",Toast.LENGTH_SHORT).show();
+                        Log.i("Volley",response.toString());
+                    }
+                }, new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        error.printStackTrace();
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
             }
         });
 //        end of adding action on button click
 
 
         return root;
+    }
+
+    public void volleyPostAddAccount(String email, String pass){
+        String postUrl="http://localhost:8080/account/add";
+        RequestQueue requestQueue = Volley.newRequestQueue(this.getContext());
+
+        JSONObject postData = new JSONObject();
+        try{
+            postData.put("email",email);
+            postData.put("password", pass);
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,postUrl,postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //add toast for success
+                //Toast.makeText(UserSettingsFragment.getContext(), "Post Successful",Toast.LENGTH_SHORT).show();
+                System.out.println(response);
+                Log.i("UserSettingsFragment","this is onResponse");
+            }
+        }, new Response.ErrorListener(){
+            @Override
+                    public void onErrorResponse(VolleyError error){
+                        error.printStackTrace();
+            }
+        });
     }
 
     @Override
