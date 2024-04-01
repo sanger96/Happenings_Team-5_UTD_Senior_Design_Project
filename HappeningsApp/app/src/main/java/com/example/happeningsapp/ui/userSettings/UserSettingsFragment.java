83ad5a3f4e8a3ce4253da2527e2dd7be36dc7cb0
Gaurav <gauravkumarsanger@gmail.com>
2024-03-31 23:01:47 -0500

    all events are showing on events list page, but table needs better formatting.

--- HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/userSettings/UserSettingsFragment.java
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/userSettings/UserSettingsFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
