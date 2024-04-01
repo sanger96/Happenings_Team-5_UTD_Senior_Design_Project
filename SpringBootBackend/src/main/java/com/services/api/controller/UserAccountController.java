8e767d35cc3296bfc636e3badf2ce55ddd68c47d
Gaurav <gauravkumarsanger@gmail.com>
2024-04-01 12:58:34 -0500

    Changed checkLogin to PostMapping.

--- SpringBootBackend/src/main/java/com/services/api/controller/UserAccountController.java
+++ SpringBootBackend/src/main/java/com/services/api/controller/UserAccountController.java
@@ -55,7 +55,7 @@ public class UserAccountController {
         return appointments;
     }
 
-    @GetMapping("/checkLogin")
+    @PostMapping("/checkLogin")
     public Integer checkLogin(@RequestBody UserAccount acc){
         return service.checkLogin(acc.getEmail(), acc.getPassword());
     }
