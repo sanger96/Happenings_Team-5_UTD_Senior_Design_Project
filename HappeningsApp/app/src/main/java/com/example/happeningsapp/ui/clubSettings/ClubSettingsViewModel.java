6195a3edb782d8cb150155b4233ceea17e0cba53
Gaurav <gauravkumarsanger@gmail.com>
2024-04-07 14:43:31 -0500

    Adding places for individual event page, individual club page, individual user page, club creation form, club editing form (clubSettings).
    All pages will have folder in ui for model and views, I know that this folder shouldn't be called ui but changing it now will cause too many issues. (has both views and models)
    
    All pages will also have layouts.
    
    I am attempting to leaves some useful code on all of the pages, as well as some comments.

---
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/clubSettings/ClubSettingsViewModel.java
@@ -0,0 +1,9 @@
+package com.example.happeningsapp.ui.clubSettings;
+
+import androidx.lifecycle.ViewModel;
+
+public class ClubSettingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+    // put mutable text fields from layout here, including initialization getters and setters
+    // See other ViewModel.java like the one for login for comparison
+}
---
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/ui/individualClub/IndividualClubViewModel.java
@@ -0,0 +1,9 @@
+package com.example.happeningsapp.ui.individualClub;
+
+import androidx.lifecycle.ViewModel;
+
+public class IndividualClubViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+    // put mutable text fields from layout here, including initialization getters and setters
+    // See other ViewModel.java like the one for login for comparison
+}
