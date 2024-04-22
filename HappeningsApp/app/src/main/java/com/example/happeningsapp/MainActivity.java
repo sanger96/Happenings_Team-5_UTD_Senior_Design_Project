927c394a2707be4b2a24a42f1dd0dbd156dfa82f
LKASTI <lkastiman@gmail.com>
2024-04-21 19:46:24 -0500

    Added padding to settings page and enforced light mode

--- HappeningsApp/app/src/main/java/com/example/happeningsapp/MainActivity.java
+++ HappeningsApp/app/src/main/java/com/example/happeningsapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
